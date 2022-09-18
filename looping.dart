be903311369e12b00c463f91d45229e6fe48b1fc
RissAgung <risqiagungg246@gmail.com>
2022-09-18 06:17:15 -0700

    tugas wirewolf2

---
+++ looping.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  for (var i = 1; i <= 10; i++) {
+    print("looping maju ke-$i");
+  }
+
+  var i = 10;
+  while (i >= 0) {
+    print("looping mundur ke-$i");
+    i--;
+  }
+}
